7f911aca8ee6e424322dc2384df90b22a77aa448
Your Name <913105698@qq.com>
2019-05-29 19:27:08 +0800

    第二个项目：练习使用ViewPager

---
+++ app/src/main/java/com/swufe/wy/page/MainActivity.java
@@ -0,0 +1,23 @@
+package com.swufe.wy.page;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewpager); //viewPager控件指定区域显示Fragment，具体显示哪一个Fragment由MyPageAdapter管理
+        //获取pageAdapter对象
+        MyPageAdapter pageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager); //将tab与viewPager绑定
+
+    }
+}
